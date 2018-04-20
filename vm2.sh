4fe49709dd1b9f0f6c3cb196fbab09ef18a4b042
dndman <alexey.kalenichenko@gmail.com>
2018-04-20 03:53:53 +0300

    37

--- vm2.sh
+++ vm2.sh
@@ -45,5 +45,8 @@ route add default gw $GW_IP
 apt-get update
 apt-get -y install apache2
 
+sed -i 's/*:80/$APACHE_VLAN_IP:80/' /etc/apache2/sites-available/000-default.conf
+
+service apache2 restart
 
 
