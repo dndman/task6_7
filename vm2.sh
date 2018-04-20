2572134ed9c9ce2a420924e68c99c4965fb23f8c
dndman <xlswalker@gmail.com>
2018-04-20 23:28:48 +0300

    40

--- vm2.sh
+++ vm2.sh
@@ -46,7 +46,7 @@ apt-get update
 apt-get -y install apache2
 
 sed -i 's/*:80/$APACHE_VLAN_IP:80/' /etc/apache2/sites-available/000-default.conf
-sed -i 's//$APACHE_VLAN_IP:80/'
+#sed -i 's//$APACHE_VLAN_IP:80/'
 
 service apache2 restart
 
